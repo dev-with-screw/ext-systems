ec0e8e4141d1df4f8e5f3043a8654523550ce1f8
dev-with-screw <devwithscrew@gmail.com>
2020-11-21 13:17:41 +0200

    Add Rest

--- city-register/src/main/java/home/work/city/web/CheckPersonService.java
+++ city-register/src/main/java/home/work/city/web/CheckPersonService.java
@@ -1,5 +1,13 @@
 package home.work.city.web;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
 
+@Path("/check")
 public class CheckPersonService {
+    @GET
+    public String checkPerson() {
+        return "Simple String";
+    }
+
 }
